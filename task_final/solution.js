091bfcd04571ece070335711752850311f720aba
TSkoraya <tskoraya@mail.ru>
2022-05-20 22:48:33 +0300

    Final_task done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,11 +34,7 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client:{name, phone}, order: {address,sum}, goods: [{title, count}]};
-
-    data.client.name = ${client.name} + ' ' + ${client.phone};
-    data.order.address = 'ул. ' + ${address.street} + ', дом ' + ${address.house} + ', ' + ${address.entrance} + ' подъезд, ' + ${address.floor} + ' этаж, кв ' + ${address.flat};
-    data.order.sum = sum;
+    let data = {goods: [], order: {}};
 
     let countOfGoods = goods.length;
 
@@ -46,6 +42,10 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push({title:goods[i].title, count: goods[i].count});
     }
   
+    data.client = name + ` ` + phone;
+    data.order.address = `ул. ` + address.street + `, дом ` + address.house + `, ` + address.entrance + ` подъезд, ` + address.floor + ` этаж, кв ` + address.flat;
+    data.order.sum = sum;
+
     let jsonData = JSON.stringify({data});
 
     return jsonData;
